55daf9212a130b1ba9831fef5b17844d519bf4af
soon9 <81350489+hsk980911@users.noreply.github.com>
2022-12-08 01:03:40 +0900

    7th Miniproject

--- 7th_miniproject/Aivle_7th_miniproject/signlanguage/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-
-app_name = 'signlanguage'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('upload', views.upload, name='upload'),
-]
